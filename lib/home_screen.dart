02cb53930da34187e314498f2e62ce4db19a22f4
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-08-02 18:25:22 +0530

    Initial Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login Screen")),
+      appBar: AppBar(title: Text("Login Screen",style: TextStyle(color: Colors.blue.shade500,fontSize: 20),)),
+
     );
   }
 }
